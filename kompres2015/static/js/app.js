e7680935f87fc6e54938331f338bd55611d93b45
abirafdirp <abirafdiraditya@gmail.com>
2015-11-30 14:09:45 +0700

    remove hardcoded url in rest-auth

--- kompres2015/static/js/app.js
+++ kompres2015/static/js/app.js
@@ -112,10 +112,7 @@ angular.module('angularDjangoRegistrationAuthApp', [
         redirectTo: '/'
       });
       $LocationProvider.html5Mode(true);
-  }])
-  .run(function(djangoAuth){
-    djangoAuth.initialize('//127.0.0.1:8000/rest-auth', false);
-  });
+  }]);
 
 var kompresApp = angular.module('kompres', [
   'ngRoute',
