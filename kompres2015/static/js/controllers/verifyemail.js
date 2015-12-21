022ba27b1afd1a75ae28532bd3742e0fd69f9ac7
Abirafdi Raditya Putra <abirafdiraditya@gmail.com>
2015-12-21 07:58:11 +0700

    polish verify email

--- kompres2015/static/js/controllers/verifyemail.js
+++ kompres2015/static/js/controllers/verifyemail.js
@@ -3,9 +3,12 @@
 angular.module('angularDjangoRegistrationAuthApp')
   .controller('VerifyemailCtrl',['$scope', '$routeParams', 'djangoAuth',
     function ($scope, $routeParams, djangoAuth) {
+      $scope.show_loading = true;
       djangoAuth.verify($routeParams["emailVerificationToken"]).then(function(data){
+        $scope.show_loading = false;
         $scope.success = true;
       },function(data){
+        $scope.show_loading = false;
         $scope.failure = false;
       });
     }]);
